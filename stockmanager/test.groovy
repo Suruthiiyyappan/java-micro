df7ce9c72bcfb5c0bd875419ec877e246aa49971
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 23:24:40 +0530

    Update test.groovy

--- stockmanager/test.groovy
+++ stockmanager/test.groovy
@@ -22,9 +22,20 @@ def archiveJar() {
 }
 
 def sonar() {
-    sh "mvn clean verify sonar:sonar \
-    -Dsonar.host.url=$sonar_url \
-    -Dsonar.projectName=ss"
+    def mvnCmd = "/opt/apache-maven-3.9.5/bin/mvn"
+
+    // Use withEnv to set the PATH environment variable
+    withEnv(["PATH+MAVEN=/opt/apache-maven-3.9.5/bin"]) {
+        withSonarQubeEnv(credentialsId: 'sonar', installationName: 'sonarqube') {
+            def sonar_url = 'http://3.97.203.42:9000/'
+            def sonar_name = 'goov'
+
+            sh """${mvnCmd} clean verify sonar:sonar \\
+            -Dsonar.host.url=${sonar_url} \\
+            -Dsonar.projectName=ss"""
+        }
     }
+}
+
 
 return this
