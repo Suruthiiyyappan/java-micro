3e04da5d7df5472775f0102e173ca2c296dc1c04
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 21:33:28 +0530

    Create test.groovy

---
+++ stockmanager/test.groovy
@@ -0,0 +1,20 @@
+def mvnbuild() {
+    if (ENV == 'mule') {
+        sh 'mvn clean package'
+        archiveJar()
+    } else {
+        echo 'not build'
+    }
+}
+
+def archiveJar() {
+    archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
+}
+
+def sonar() {
+    sh "mvn clean verify sonar:sonar \
+    -Dsonar.host.url=$sonar_url \
+    -Dsonar.projectName=ss"
+    }
+
+return this
