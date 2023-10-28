b7f7b5460051ba70548e6d2f123f985003bf26e8
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 15:36:50 +0530

    Update sample.groovy

--- sample.groovy
+++ sample.groovy
@@ -1,34 +1,23 @@
 def mvnbuild() {
-    try {
-        sh "mvn clean package"
-    } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        error "Maven build failed: ${e.message}"
-    }
+    sh "mvn clean package"
 }
 
 def sonar() {
-    try {
-        withSonarQubeEnv(credentialsId: 'sonar', installationName: 'sonarqube') {
-            sh "mvn clean verify sonar:sonar -Dsonar.host.url=$sonar_url -Dsonar.projectName=$sonar_name"
-        }
-    } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        error "SonarQube analysis failed: ${e.message}"
+    withSonarQubeEnv(credentialsId: 'sonar', installationName: 'sonarqube') {
+        sh "mvn clean verify sonar:sonar -Dsonar.host.url=$sonar_url -Dsonar.projectName=$sonar_name"
     }
 }
 
 def qualitygate() {
     def qg = waitForQualityGate()
     if (qg.status != 'OK') {
-        currentBuild.result = 'FAILURE'
         error "Quality gate not OK: ${qg.status}"
     }
 }
 
 def approval() {
     def approvalEmail = mail(
-        subject: "Approve for Build ${env.BUILD_NUMBER}",
+        subject: "approve for ${env.BUILD_NUMBER}",
         body: """${env.JOB_URL}/${env.BUILD_NUMBER}/input/">Approve / Reject </a> this build to import in the Dev Fabric.. <br>""",
         mimeType: 'text/html',
         to: "suruthi.iyappan@gmail.com"
@@ -37,20 +26,16 @@ def approval() {
     timeout(time: 1, unit: 'HOURS') {
         def approvalInput = input(
             id: 'approvalInput',
-            message: 'Approve or reject the deployment to DEV',
+            message: 'Approve or reject the for DEV',
             parameters: [choice(choices: ['Approve', 'Reject'], description: 'Choose to approve or reject', name: 'approvalChoice')],
             submitter: 'admin'
         )
 
+        // Get the selected value from the input
         def approvalChoice = approvalInput.approvalChoice
 
         if (approvalChoice == 'Approve') {
-            try {
-                echo "app"
-            } catch (Exception e) {
-                currentBuild.result = 'FAILURE'
-                error "app failed: ${e.message}"
-            }
+            sh "sudo aws s3 cp stockmanager-0.0.1-SNAPSHOT.jar s3://suruthi1/artifacts/ --acl bucket-owner-full-control"
         } else {
             error('Deployment aborted by user')
         }
