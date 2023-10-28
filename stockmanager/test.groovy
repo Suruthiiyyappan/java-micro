ce2f2cbc4df8e7aa3259f5da6abb65980d0bb5b0
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-28 22:51:22 +0530

    Update test.groovy

--- stockmanager/test.groovy
+++ stockmanager/test.groovy
@@ -1,11 +1,16 @@
 def mvnbuild(ENV) {
     if (ENV == 'mule') {
-        sh 'mvn clean package'
-        archiveJar()
+        script {
+            def mvnHome = tool name: 'maven-3.9.5', type: 'Maven'
+            def mvnCmd = "${mvnHome}/bin/mvn"
+            sh "${mvnCmd} clean package"
+            archiveJar()
+        }
     } else {
         echo 'not build'
     }
 }
+
 
 def archiveJar() {
     archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
