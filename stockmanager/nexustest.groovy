1db96f9a0022b75a3ea1b2fc034ea340283a98de
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-29 20:26:23 +0530

    Update nexustest.groovy

--- stockmanager/nexustest.groovy
+++ stockmanager/nexustest.groovy
@@ -23,7 +23,7 @@ def nexus() {
 
     // Use withEnv to set the PATH environment variable
     withEnv(["PATH+MAVEN=/opt/apache-maven-3.9.5/bin"]) {
-        withCredentials([usernamePassword(credentialsId: 'nexus3', usernameVariable: 'username', passwordVariable: 'password', maskPasswords: true)]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'mycreds', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
             sh """
                 ${mvnCmd} deploy:deploy-file \
                 -Dfile=target/stockmanager-0.0.1-SNAPSHOT.jar \
@@ -32,9 +32,9 @@ def nexus() {
                 -Dpackaging=jar \
                 -DrepositoryId=maven-snapshots \
                 -Durl=http://3.99.33.174:8081/repository/maven-snapshots/ \
-                -DgroupId=uk.co.danielbryant.djshopping
-                -Dusername=\$username
-                -Dpassword=\$password
+                -DgroupId=uk.co.danielbryant.djshopping \
+                -Dusername=$USERNAME \
+                -Dpassword=$PASSWORD
             """
         }
     }
