5c7602c2c58f7834953cd684aefd40acf1dbf2aa
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-29 21:46:38 +0530

    Update nexustest.groovy

--- stockmanager/nexustest.groovy
+++ stockmanager/nexustest.groovy
@@ -42,7 +42,7 @@ def nexus() {
             protocol: "http",
             nexusUrl: "http://3.99.33.174:8081/repository/maven-snapshots",
             groupId: "uk.co.danielbryant.djshopping",
-            version: "${BUILD_NUMBER}",
+            version: "0.0.1-SNAPSHOT",
             repository: "maven-snapshots",
             credentialsId: "nexus3",
             artifacts: [
