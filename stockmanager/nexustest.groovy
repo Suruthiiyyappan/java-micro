fe77f98a4b3aefdc2d16293e92cd50ac5da72fb4
Suruthi <96717233+Suruthiiyyappan@users.noreply.github.com>
2023-10-29 20:51:49 +0530

    Update nexustest.groovy

--- stockmanager/nexustest.groovy
+++ stockmanager/nexustest.groovy
@@ -7,6 +7,7 @@ def mvnbuild(ENV) {
             withEnv(["PATH+MAVEN=/opt/apache-maven-3.9.5/bin"]) {
                 sh "${mvnCmd} clean package"
                 archiveJar()
+                publishToNexus()
             }
         }
     } else {
@@ -18,27 +19,47 @@ def archiveJar() {
     archiveArtifacts artifacts: '**/target/*.jar', allowEmptyArchive: true
 }
 
-def nexus() {
-    def mvnCmd = "/opt/apache-maven-3.9.5/bin/mvn"
+def publishToNexus() {
+    script {
+        def nexusPublisher = NexusPublisher.newInstance()
 
-    // Use withEnv to set the PATH environment variable
-    withEnv(["PATH+MAVEN=/opt/apache-maven-3.9.5/bin"]) {
-        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'nexus3', usernameVariable: 'nexus3_Username', passwordVariable: 'nexus3_Password']]) {
-            sh """
-                ${mvnCmd} deploy:deploy-file \
-                -Dfile=target/stockmanager-0.0.1-SNAPSHOT.jar \
-                -DartifactId=stockmanager \
-                -Dversion=0.0.1-SNAPSHOT \
-                -Dpackaging=jar \
-                -DrepositoryId=maven-snapshots \
-                -Durl=http://3.99.33.174:8081/repository/maven-snapshots/ \
-                -DgroupId=uk.co.danielbryant.djshopping \
-                -Dusername=$nexus3_Username \
-                -Dpassword=$nexus3_Password
-            """
-        }
+        nexusPublisher.nexusUrl = 'http://3.99.33.174:8081/repository/maven-snapshots/' // Replace with your Nexus URL
+        nexusPublisher.nexusCredentialsId = 'nexus3' // Replace with your Nexus credentials ID
+        nexusPublisher.repositoryName = 'maven-snapshots' // Replace with your Nexus repository name
+        nexusPublisher.asset = [
+            [
+                groupId: 'uk.co.danielbryant.djshopping',
+                artifactId: 'stockmanager',
+                version: '0.0.1-SNAPSHOT',
+                packaging: 'jar',
+                file: 'target/stockmanager-0.0.1-SNAPSHOT.jar'
+            ]
+        ]
+        
+        nexusPublisher.perform()
     }
 }
-
-
 return this
+
+// def nexus() {
+//     def mvnCmd = "/opt/apache-maven-3.9.5/bin/mvn"
+
+//     // Use withEnv to set the PATH environment variable
+//     withEnv(["PATH+MAVEN=/opt/apache-maven-3.9.5/bin"]) {
+//         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'nexus3', usernameVariable: 'nexus3_Username', passwordVariable: 'nexus3_Password']]) {
+//             sh """
+//                 ${mvnCmd} deploy:deploy-file \
+//                 -Dfile=target/stockmanager-0.0.1-SNAPSHOT.jar \
+//                 -DartifactId=stockmanager \
+//                 -Dversion=0.0.1-SNAPSHOT \
+//                 -Dpackaging=jar \
+//                 -DrepositoryId=maven-snapshots \
+//                 -Durl=http://3.99.33.174:8081/repository/maven-snapshots/ \
+//                 -DgroupId=uk.co.danielbryant.djshopping \
+//                 -Dusername=$nexus3_Username \
+//                 -Dpassword=$nexus3_Password
+//             """
+//         }
+//     }
+// }
+
